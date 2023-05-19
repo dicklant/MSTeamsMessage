705524ed2ffbeb0069253b090a86d6f2844cb372
dicklant <dicklant@hotmail.com>
2023-05-19 17:30:46 +0200

    Add MS Teams JS

--- js/tabconfig.js
+++ js/tabconfig.js
@@ -1,4 +1,4 @@
-import {microsoftTeams} from "@microsoft/teams-js"
+//import {microsoftTeams} from "@microsoft/teams-js"
 
 function btnPush() {
     let codeSession = document.getElementById("idSessionCode").value;
@@ -7,23 +7,4 @@ function btnPush() {
     window.location.href = "tab.html?Session=" + codeSession;
 }
 
-function InitMSTeam() {
-    microsoftTeams.app.initialize().then(() => {
-        microsoftTeams.pages.config.registerOnSaveHandler((saveEvent) => {
 
-            const tabName = document.getElementById("idTabName").value;
-            const sessionCode = document.getElementById("idSessionCode").value;
-
-            microsoftTeams.pages.config.setConfig({
-                suggestedDisplayName: tabName,
-                entityId: "Yellow",
-                contentUrl: "tab.html?Session=" + codeSession,
-                websiteUrl: "https://yellowintegration.azurewebsites.net",
-            }).then(() => {
-                saveEvent.notifySuccess();
-            });
-        });
-
-        microsoftTeams.pages.config.setValidityState(true);
-    });
-}
